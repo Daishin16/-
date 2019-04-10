0dcb33d71bde1dba661e913419d17387e54c7052
izacchi16 <sakuya1202@icloud.com>
2019-04-10 12:33:36 +0900

    SecondCommit

--- Files/FizzBuzz/Main.java
+++ Files/FizzBuzz/Main.java
@@ -1,5 +1,30 @@
+/*
+1から100までを出力対象としています
+製作開始日2019/04/10
+*/
+
 public class Main {
     public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(start(i));
+        }
+    }
 
+    static String start(int i) {
+        String result = "";
+
+        if (i % 3 == 0 && i % 5 == 0) {
+            result += "FizzBuzz";
+            return result;
+        }
+        if (i % 5 == 0) {
+            result += "Buzz";
+            return result;
+        }
+        if (i % 3 == 0) {
+            result += "Fizz";
+            return result;
+        }
+        return Integer.toString(i);
     }
 }
