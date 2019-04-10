21ca34bf9e4499af7dc575a3af676a5e58349f45
izacchi16 <sakuya1202@icloud.com>
2019-04-10 17:49:25 +0900

    Firstcommit

---
+++ CalcStats/java/Main.java
@@ -0,0 +1,29 @@
+/*
+2019/4/10 started
+
+ */
+import java.util.ArrayList;
+
+public class Main {
+    static ArrayList<Integer> list = new ArrayList<Integer>();
+
+    public static void main(String[] args) {
+        initialize();
+    }
+
+    /*
+    add 10 elements
+    */
+    public static void initialize() {
+        list.add(1);
+        list.add(3);
+        list.add(-36);
+        list.add(0);
+        list.add(10);
+        list.add(-3);
+        list.add(5);
+        list.add(100);
+        list.add(-56);
+        list.add(7);
+    }
+}
