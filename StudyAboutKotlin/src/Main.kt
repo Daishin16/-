fca91197c1f02421028112071c98073868a682b4
Daishin16 <sakuya1202@icloud.com>
2019-06-13 12:18:38 +0900

    2019/06/13

--- StudyAboutKotlin/src/Main.kt
+++ StudyAboutKotlin/src/Main.kt
@@ -4,9 +4,32 @@
 
 */
 
+class Person(
+    val name: String,
+    var isMarried: Boolean
+)
+
+class Rectangle(val height: Int, val width: Int) {
+    val isSquare: Boolean
+        get() {
+            return height == width
+        }
+}
+
 fun main(args: Array<String>) {
     println("Hello,World!")
-    println(max(1, 2))
+    println("fun max result => " + max(1, 2))
+
+    val languages = arrayListOf<String>("Java")
+    languages.add("Kotlin")
+
+    val person = Person("Bob", true)
+    println("person.name => " + person.name)
+    println("peeson.isMarried => " + person.isMarried)
+
+    val rectangle = Rectangle(41, 43)
+    println("isRectangle? => " + rectangle.isSquare)
+
 }
 
 fun max(a: Int, b: Int): Int {
