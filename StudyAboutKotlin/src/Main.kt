606e23234a5b7a3608df84f37715e6bcebca6a7e
Daishin16 <sakuya1202@icloud.com>
2019-06-19 17:33:06 +0900

    2019/06/19

--- StudyAboutKotlin/src/Main.kt
+++ StudyAboutKotlin/src/Main.kt
@@ -9,18 +9,18 @@ class Person(
     var isMarried: Boolean
 )
 
-class Rectangle(val height: Int, val width: Int) {
+class Rectangle(private val height: Int, private val width: Int) {
     val isSquare: Boolean
         get() {
             return height == width
         }
 }
 
-fun main(args: Array<String>) {
+fun main() {
     println("Hello,World!")
     println("fun max result => " + max(1, 2))
 
-    val languages = arrayListOf<String>("Java")
+    val languages = arrayListOf("Java")
     languages.add("Kotlin")
 
     val person = Person("Bob", true)
@@ -35,6 +35,8 @@ fun main(args: Array<String>) {
 //    println("RED&BLUE mixed => " + mix(Color.RED, Color.BLUE)) => Exception
     println("RED&YELLOW mixed => " + mix(Color.RED, Color.YELLOW))
     println("YELLOW&RED mixed => " + mix(Color.YELLOW, Color.RED))//順番は関係しない
+
+    println("eval() result =>" + eval(Sum(Sum(Num(1),Num(2)),Num(4))))
 
 }
 
@@ -61,4 +63,18 @@ fun mix(c1: Color, c2: Color) = when (setOf(c1, c2)) {
     setOf(Color.YELLOW, Color.BLUE) -> Color.GREEN
     setOf(Color.BLUE, Color.VIOLET) -> Color.INDIGO
     else -> throw Exception("Dirty color")
+}
+
+interface Expr
+class Num(val value: Int) : Expr
+class Sum(val left: Expr, val right: Expr) : Expr
+
+fun eval(e: Expr): Int {
+    if (e is Num) {
+        return e.value
+    }
+    if (e is Sum) {
+        return eval(e.right) + eval(e.left)
+    }
+    throw IllegalArgumentException("Unknown expression")
 }
